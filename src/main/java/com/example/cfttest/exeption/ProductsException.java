4aebc502c9656fb4b6d668bb704cb9557bf02a57
zertalian <Zertalian@mail.ru>
2023-02-19 18:14:08 +0700

    completed task

--- src/main/java/com/example/cfttest/exeption/ProductsException.java
+++ src/main/java/com/example/cfttest/exeption/ProductsException.java
@@ -1,11 +1,9 @@
 package com.example.cfttest.exeption;
 
 public class ProductsException extends RuntimeException{
-
     public ProductsException(){
         super("Undescribed error");
     }
-
     public ProductsException(String text){
         super(text);
     }
